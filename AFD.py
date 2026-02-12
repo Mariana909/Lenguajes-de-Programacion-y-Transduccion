047e204227957127e49d0aae741985e1305d7b48
Mariana909 <karla.duque01@usa.edu.co>
2026-02-12 13:38:29 +0000

    Add files via upload

---
+++ AFD.py
@@ -0,0 +1,28 @@
+import sys
+
+def afd(entrada):
+	#Transiciones
+	trans={	 'q1': {'0': 'q2', '1': 'q3'},
+		 'q2': {'0': 'q2', '1': 'q2'},
+		 'q3': {'0': 'q3', '1': 'q3'}
+	      }
+	# El estado inicial es q1
+	q='q1' 
+	for e in entrada:
+		q=trans[q][e]
+	if q == 'q2':
+		print("ACEPTADO")
+	else:
+		print("NO ACEPTADO")
+try:
+	if len(sys.argv) > 1:
+		entrada = sys.argv[1]
+		with open(entrada, 'r') as en:
+			datos = en.read().split()
+			for i in datos:
+				afd(i)
+	else:
+		print("No se detectó archivo de entrada")
+
+except:
+	print(" No se encontró el archivo")
