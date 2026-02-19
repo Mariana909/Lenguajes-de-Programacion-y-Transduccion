661a5eb0f6a0b40c161e256f580adba507616c27
Mariana909 <karla.duque01@usa.edu.co>
2026-02-18 19:24:09 -0500

    Create ejercicio6.c

---
+++ Ejercicios-Flex-y-Bison/ejercicio6.c
@@ -0,0 +1,31 @@
+/* wc_manual.c */
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int chars = 0;
+    int words = 0;
+    int lines = 0;
+    int in_word = 0;
+    int c;
+    
+    while ((c = getchar()) != EOF) {
+        chars++;
+        
+        if (c == '\n') {
+            lines++;
+        }
+        
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+            if (!in_word) {
+                words++;
+                in_word = 1;
+            }
+        } else {
+            in_word = 0;
+        }
+    }
+    
+    printf("%8d%8d%8d\n", lines, words, chars);
+    return 0;
+}
